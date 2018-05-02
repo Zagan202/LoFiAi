a946efc693e5fffbb4d83a237d60e9e617a55fef
Alex Perrotti <alexperrotti@gmail.com>
2018-05-01 19:00:20 -0700

    random autoplay works

--- back-end/server/routes/songs.js
+++ back-end/server/routes/songs.js
@@ -3,30 +3,30 @@ var router = express.Router();
 var Song = require("../models/songs"); // comment model
 
 router.route("/get/song")
-	.get(function(req, res) {
-		// looks at our Songs Schema
-		Song.find(function(err, songs) {
-			if (err)
-				res.send(err);
-				
-			console.log(songs);
-			var  randomIndex = Math.floor(Math.random() * songs.length);
-			// Set response status to 200 if successful
-			// 400 if there is an error
-			// responds with a json object of the song path at randomIndex.
-			res.status(200).send(songs);
-		});
-	})
+  .get(function(req, res) {
+    // looks at our Songs Schema
+    Song.find(function(err, songs) {
+      if (err)
+        res.send(err);
+      //console.log(songs);
+	  //var  randomIndex = Math.floor(Math.random() * songs.length);
+	  
+      // Set response status to 200 if successful
+      // 400 if there is an error
+      // responds with a json object of the song path at randomIndex.
+      res.status(200).send(songs);
+    });
+  })
 
-	// adds a new song to the database 
-	.post(function(req, res) {
-		var song = new Song();
-		song.path = req.body.path;
-		song.save(function(err) {
-			if(err)
-				res.status(400).send(err);
-			res.status(200).send({ message: "Song successfully added!" });
-		});
-	});
+  // adds a new song to the database 
+  .post(function(req, res) {
+    var song = new Song();
+    song.path = req.body.path;
+    song.save(function(err) {
+      if(err)
+        res.status(400).send(err);
+      res.status(200).send({ message: "Song successfully added!" });
+    });
+  });
 
 module.exports = router;
