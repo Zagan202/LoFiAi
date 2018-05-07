58907c1b0d85a2e100f292cafe7bddb2e95fdd52
chesmend <38272982+chesmend@users.noreply.github.com>
2018-05-07 02:07:59 -0700

    Add files via upload
    updated nav bar to have hoverable links (not sticky)

--- lofiai/src/App.js
+++ lofiai/src/App.js
@@ -4,16 +4,26 @@ import Song from './Song'
 import './styles.css';
 
 class App extends Component {
+
+
+	
   render() {
+	  
     return (
-      <div className="App">
-      <div className = "top">LoFiAi (Top Navigation?)</div>
-          <img src={logo} className="App-logo" alt="logo" />
+	
+      <div className = "App">
+      <img src={logo} className="App-logo" alt="logo" />
+	  <div className = "navbar">
+      <a class = "active" href = "javascript:void(0)">Home</a>
+      <a href = "javascript:void(0)">Player</a>
+      <a href = "javascript:void(0)">About</a>
+      </div>
       <div className = "mid">
-        <Song url="http://localhost:4200/get/song" pollInterval={2000} />
-      </div>
+      <Song url="http://localhost:4200/get/song" pollInterval={2000} />
+      </div> {/* mid end */}
       <div className = "about">Whale whale whale</div>
-      </div>
+      </div> /* app end */
+
     );
   }
 }
