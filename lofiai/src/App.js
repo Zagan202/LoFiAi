f1af9d443701839da6d70fa37d87d800f9ac857b
Alex Perrotti <alexperrotti@gmail.com>
2018-05-10 17:21:41 -0700

    sticky

--- lofiai/src/App.js
+++ lofiai/src/App.js
@@ -4,25 +4,45 @@ import Song from './Song'
 import './styles.css';
 
 class App extends Component {
+  constructor(props){
+    super(props);
+    this.state = {mounted: false};
+    //this.checkSticky = this.checkSticky.bind(this);
+  }
 
+  componentDidMount(){
+    window.onscroll = function() {checkSticky()};
 
+    // Checks if navbar is offset enough to stick to the top
+    function checkSticky(e){
+      var navbarElement = document.getElementById("nav");
+      console.log(navbarElement);
+      var stickHeight = navbarElement.offsetTop;
+      if (window.pageYOffset >= stickHeight) {
+        navbarElement.classList.add("sticky")
+      } else {
+        navbarElement.classList.remove("sticky");
+      }
+    }
+  }
 	
   render() {
-	  
+    
+    
     return (
 	
       <div className = "App">
-      <img src={logo} className="App-logo" alt="logo" />
-	  <div className = "navbar">
-      <a class = "active" href = "javascript:void(0)">Home</a>
-      <a href = "javascript:void(0)">Player</a>
-      <a href = "javascript:void(0)">About</a>
-      </div>
-      <div className = "mid">
-      <Song url="http://localhost:4200/get/song" pollInterval={2000} />
-      </div> {/* mid end */}
-      <div className = "about">Whale whale whale</div>
-      </div> /* app end */
+        <img id = "logo" src={logo} className="App-logo" alt="logo" />
+	      <div id = "nav" className = "navbar">
+          <a className = "active" href = "#logo">Home</a>
+          <a href = "#mid" >Player</a>
+          <a href = "#about">About</a>
+        </div>
+        <div id="mid" className = "mid">
+          <Song url="http://localhost:4200/get/song"/>
+        </div> {/* mid end */}
+        <div id="about" className = "about">Whale whale whale</div>
+        </div> /* app end */
 
     );
   }
