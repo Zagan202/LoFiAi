4ebbf66201317dcc57aae85e1d529e69c7b98637
Alex Perrotti <alexperrotti@gmail.com>
2018-05-11 12:21:29 -0700

    implemented social media and clipboard functionality

--- front-end/src/Share.js
+++ front-end/src/Share.js
@@ -5,23 +5,68 @@ import './styles.css';
 class Share extends Component{
   constructor(props){
     super(props);
-    this.press = this.press.bind(this);
+    this.getShareLink = this.getShareLink.bind(this);
+    this.copyLink = this.copyLink.bind(this);
+    this.tweet = this.tweet.bind(this);
+    this.face = this.face.bind(this);
   }
 
-  // Creates link with first parameter set to curent song
-  press(){
+  // Returns a link to our website at the current song
+  getShareLink(){
     var shareLink = window.location.href;
     shareLink = shareLink.split('?')[0];
     shareLink = (shareLink + "?first=" + String(this.props.index));
-    // Below will be replaced with presenting the user with shareLink for social media
-    console.log(shareLink);
+    return(shareLink);
+  }
+
+  // Opens link argument and focuses on it
+  open(link){
+    var win = window.open(link);
+    win.focus();
+  }
+
+  // Creates a textbox with the link and copies it to the clipboard
+  copyLink(){
+    var link = this.getShareLink();
+    var input = document.createElement("input");
+    var element = document.getElementById("linkdiv");
+    element.appendChild(input);
+    input.value = link;
+    input.select();
+    document.execCommand("Copy");
+  }
+
+  // Opens a tweet sharing our website
+  tweet(){
+    var link = this.getShareLink();
+    // Format as a twitter link
+    link = ("https://twitter.com/intent/tweet?url=" + link);
+    link = (link + "&text=Check%20out%20this%20sick%20AI%20generated%20music%21");
+    this.open(link);
+  }
+
+  // Opens a facebook post sharing our website
+  face(){
+    var link = this.getShareLink();
+    // Format as a facebook link
+    link = ("https://www.facebook.com/sharer/sharer.php?u=" + link);
+    // This currently will give an error,
+    //  from what I've deduced, this is because facebook doesn't share links
+    //  to local host. It should be fixed when we are hosted properly
+    this.open(link);
   }
 
   // Renders buttons which trigger press
   render(){
     return(
       <div>
-        <button onClick={this.press}>Share</button>
+        <div>
+          <button onClick={this.copyLink}>Copy Link To Clipboard</button>
+          <button onClick={this.tweet}>Twitter</button>
+          <button onClick={this.face}>Facebook</button>
+        </div>
+        <div id="linkdiv">
+        </div>
       </div>
     )
   }
